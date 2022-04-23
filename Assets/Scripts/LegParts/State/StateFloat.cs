95eeae7cbd26183bc6f09058269e7c919384f26e
sorude26 <sorude26@gmail.com>
2022-04-23 17:51:14 +0900

    ホバーステートを追加した

---
+++ Assets/Scripts/LegParts/State/StateFloat.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+partial class LegController
+{
+    public class StateFloat : ILegState
+    {
+        public void OnEnter(LegController control)
+        {
+        }
+
+        public void OnFixedUpdate(LegController control)
+        {
+        }
+
+        public void OnUpdate(LegController control)
+        {
+        }
+    }
+}
