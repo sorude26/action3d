c4766eef897195238803cfd13d2e1237ce51414d
sorude26 <sorude26@gmail.com>
2022-04-23 16:43:12 +0900

    待機と歩行のステートを追加した

---
+++ Assets/Scripts/LegParts/State/StateIdle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+partial class LegController
+{
+    public class StateIdle : ILegState
+    {
+        public void OnEnter(LegControl control)
+        {
+        }
+
+        public void OnFixedUpdate(LegControl control)
+        {
+        }
+
+        public void OnUpdate(LegControl control)
+        {
+        }
+    }
+}
