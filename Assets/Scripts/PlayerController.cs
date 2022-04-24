9a5af414de504200f26edbe8b164161b20b32353
sorude26 <sorude26@gmail.com>
2022-04-24 18:31:55 +0900

    プレイヤー操作用スクリプトを追加した

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    MachineController _controller = default;
+    void Start()
+    {
+        GameScene.InputManager.Instance.OnInputAxisRaw += _controller.InputMove;
+        GameScene.InputManager.Instance.OnFirstInputJump += _controller.InputJump;
+        GameScene.InputManager.Instance.OnInputAxisRawExit += _controller.MoveEnd;
+    }
+}
