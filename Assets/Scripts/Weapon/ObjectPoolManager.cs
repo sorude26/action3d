7d1b1182234b51f205afda94ab238566bcf35308
sorude26 <sorude26@gmail.com>
2022-05-09 18:05:05 +0900

    オブジェクトプール使用時にそのオブジェクトのリストがない場合にリスト生成を行う処理を追加した

--- Assets/Scripts/Weapon/ObjectPoolManager.cs
+++ Assets/Scripts/Weapon/ObjectPoolManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ObjectPoolManager : MonoBehaviour
 {
+    private const int DEFAULT_POOL_COUNT = 5;
     private static ObjectPoolManager s_instance = default;
     private static Dictionary<string,List<PoolObject>> s_poolObjects = default;
     public static ObjectPoolManager Instance
@@ -20,18 +21,42 @@ public class ObjectPoolManager : MonoBehaviour
             return s_instance; 
         }
     }
-    public void CreatePool(PoolObject poolObject,int createCount)
+    public void CreatePool(PoolObject poolObject,int createCount = DEFAULT_POOL_COUNT)
     {
         var poolObjects = new List<PoolObject>();
         for (int i = 0; i < createCount; i++)
         {
             var obj = Instantiate(poolObject,this.transform);
+            obj.gameObject.SetActive(false);
             poolObjects.Add(obj);
         }
         s_poolObjects.Add(poolObject.ToString(), poolObjects);
     }
+    public PoolObject Use(PoolObject useObject)
+    {
+        if (!s_poolObjects.ContainsKey(useObject.ToString())) 
+        {
+            CreatePool(useObject);
+        }
+        foreach (var poolObject in s_poolObjects[useObject.ToString()])
+        {
+            if (poolObject.gameObject.activeInHierarchy)
+            {
+                continue;
+            }
+            poolObject.gameObject.SetActive(true);
+            return poolObject;
+        }
+        var obj = Instantiate(useObject, this.transform);
+        s_poolObjects[useObject.ToString()].Add(obj);
+        return obj;
+    }
 }
 public abstract class PoolObject : MonoBehaviour
 {
-
+    public void SetPos(Vector3 pos, Vector3 dir)
+    {
+        transform.position = pos;
+        transform.forward = dir;
+    }
 }
