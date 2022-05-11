f95bf092a797200dbb2a91dc2888d4442b66e20b
sorude26 <sorude26@gmail.com>
2022-05-11 15:00:22 +0900

    爆発のエフェクト設定を行った

--- Assets/Scripts/Weapon/EffectController.cs
+++ Assets/Scripts/Weapon/EffectController.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class EffectController : MonoBehaviour
 {
     [SerializeField]
     private ParticleSystem _particle = default;
+    [SerializeField]
+    private VisualEffect _vfxEffect = default;
+    [SerializeField]
+    private float _lifeTime = 1f;
+    private float _timer = 0f;
+    private void Update()
+    {
+        if (_timer > 0)
+        {
+            _timer -= Time.deltaTime;
+            if (_timer <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
     private void OnDisable()
     {
         if (_particle != null)
@@ -19,5 +36,10 @@ public class EffectController : MonoBehaviour
         {
             _particle.Play();
         }
+        if (_vfxEffect)
+        {
+            _vfxEffect.Play();
+        }
+        _timer = _lifeTime;
     }
 }
